a7fcceaee44b2fe0cd9e4d798d58a6f7ade49f3e
Ryan Dwyer <ryan@Ryans-MacBook-Pro.local>
2016-03-03 11:56:11 -0500

    added gulp to the start sequence

--- src/js/app.js
+++ src/js/app.js
@@ -14,80 +14,6 @@ app.directive('card', function () {
 });
 'use strict';
 
-app.directive('die', function () {
-  return {
-    restrict: 'E',
-    scope: {
-      die: '=die'
-    },
-    templateUrl: '../views/dice.html'
-  };
-});
-'use strict';
-
-app.service('GameState', function () {
-  var _this = this;
-
-  this.gameStarted = false;
-  this.cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  this.dieValues = { one: 0, two: 0 };
-  this.validCards = [];
-
-  this.buildValidCards = function () {
-    var powerSet = _this.buildPowerSet();
-    var value = _this.dieValues.one + _this.dieValues.two;
-    _this.validCards = powerSet.filter(function (set) {
-      if (set[0] > value) return false;
-      return value === set.reduce(function (prev, next) {
-        return prev + next;
-      }, 0);
-    });
-  };
-
-  this.buildPowerSet = function () {
-    if (_this.cards.length === 1) return [_this.cards[0]];
-    var powerSet = [[]];
-    for (var i = 0; i < _this.cards.length; i++) {
-      for (var j = 0, len = powerSet.length; j < len; j++) {
-        powerSet.push(powerSet[j].concat(_this.cards[i]));
-      }
-    }
-    return powerSet;
-  };
-
-  this.removeCards = function (cards) {
-    for (var key in cards) {
-      if (cards.hasOwnProperty(key) && cards[key]) {
-        _this.cards.splice(key - 1, 1, '');
-      }
-    }
-  };
-
-  this.getCardsLeft = function () {
-    return _this.cards;
-  };
-
-  this.getDieValues = function () {
-    return _this.dieValues;
-  };
-
-  this.getValidChoices = function () {
-    return _this.validCards;
-  };
-
-  this.getGameState = function () {
-    return _this.gameStarted;
-  };
-
-  this.reset = function () {
-    _this.gameStarted = false;
-    _this.cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    _this.dieValues = { one: 0, two: 0 };
-    _this.validCards = [];
-  };
-});
-'use strict';
-
 app.directive('game', function () {
   return {
     restrict: 'E',
@@ -190,5 +116,79 @@ app.controller('GameLogicCtrl', function ($scope, GameState) {
     GameState.buildValidCards();
     var choices = GameState.getValidChoices();
     return choices.length === 0;
+  };
+});
+'use strict';
+
+app.directive('die', function () {
+  return {
+    restrict: 'E',
+    scope: {
+      die: '=die'
+    },
+    templateUrl: '../views/dice.html'
+  };
+});
+'use strict';
+
+app.service('GameState', function () {
+  var _this = this;
+
+  this.gameStarted = false;
+  this.cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  this.dieValues = { one: 0, two: 0 };
+  this.validCards = [];
+
+  this.buildValidCards = function () {
+    var powerSet = _this.buildPowerSet();
+    var value = _this.dieValues.one + _this.dieValues.two;
+    _this.validCards = powerSet.filter(function (set) {
+      if (set[0] > value) return false;
+      return value === set.reduce(function (prev, next) {
+        return prev + next;
+      }, 0);
+    });
+  };
+
+  this.buildPowerSet = function () {
+    if (_this.cards.length === 1) return [_this.cards[0]];
+    var powerSet = [[]];
+    for (var i = 0; i < _this.cards.length; i++) {
+      for (var j = 0, len = powerSet.length; j < len; j++) {
+        powerSet.push(powerSet[j].concat(_this.cards[i]));
+      }
+    }
+    return powerSet;
+  };
+
+  this.removeCards = function (cards) {
+    for (var key in cards) {
+      if (cards.hasOwnProperty(key) && cards[key]) {
+        _this.cards.splice(key - 1, 1, '');
+      }
+    }
+  };
+
+  this.getCardsLeft = function () {
+    return _this.cards;
+  };
+
+  this.getDieValues = function () {
+    return _this.dieValues;
+  };
+
+  this.getValidChoices = function () {
+    return _this.validCards;
+  };
+
+  this.getGameState = function () {
+    return _this.gameStarted;
+  };
+
+  this.reset = function () {
+    _this.gameStarted = false;
+    _this.cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    _this.dieValues = { one: 0, two: 0 };
+    _this.validCards = [];
   };
 });
